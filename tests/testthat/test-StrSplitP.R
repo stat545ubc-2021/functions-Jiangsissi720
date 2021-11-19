069b404388ce5f8df7385162a2c77ef34ca7ced8
Jiangsissi720 <qianhuijiang720@163.com>
2021-11-19 14:24:21 -0800

    test

--- tests/testthat/test-StrSplitP.R
+++ tests/testthat/test-StrSplitP.R
@@ -1,5 +1,5 @@
 x <- c("3D/MON&SUN2D/MON&SUN")
-test('Strsplit test!', {
+test_that('Strsplit test!', {
   expect_error(StrSplitP(StrSplitP(x, "S", type = "WILLBEERROR")))
   expect_equal(StrSplitP(x, "/", type = "remove"), list(c("3D","MON&SUN2D","MON&SUN")))
   expect_equal(StrSplitP(x,""),list(c("3", "D", "/", "M", "O", "N", "&", "S", "U", "N","2", "D", "/", "M", "O", "N", "&", "S", "U", "N")))
